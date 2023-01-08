e3c3c000f31d953740d8130410bad83012fe2e38
Vahid Ghanbarpour <vahidgr.vahid@gmail.com>
2023-01-08 14:31:50 +0330

    Initial Commit

---
+++ brooklyn/brooklynApp.swift
@@ -0,0 +1,17 @@
+//
+//  brooklynApp.swift
+//  brooklyn
+//
+//  Created by Vahid Ghanbarpour on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct brooklynApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
