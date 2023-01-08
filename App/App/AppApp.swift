fe0eab3b6e83f4fe441804018a19f3bcd40e5bc4
Vahid Ghanbarpour <vahidgr.vahid@gmail.com>
2023-01-08 15:24:05 +0330

    create app and networking modules

---
+++ App/App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Vahid Ghanbarpour on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
